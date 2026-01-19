10b0671105ef7a6347d5a315da813ca825fb2350
deannos <amjha21122002@gmail.com>
2026-01-19 06:48:32 +0530

    injector: wire metrics registration

--- cmd/injector/main.go
+++ cmd/injector/main.go
@@ -17,35 +17,62 @@ import (
 )
 
 func main() {
-	cfg := config.Load()
 	logger := log.New(os.Stdout, "", log.LstdFlags)
 
-	// Prometheus registry is global
-	// Metrics must exist before HTTP handler serves /metrics
+	logger.Println("BOOT: starting injector")
+
+	cfg := config.Load()
+	logger.Printf("BOOT: config loaded: %+v\n", cfg)
+
+	logger.Println("BOOT: registering metrics")
 	metrics.Register()
 
+	logger.Println("BOOT: creating bounded queue")
 	q := queue.NewBoundedQueue(cfg.QueueMaxRecords, cfg.QueueMaxBytes)
+
+	logger.Println("BOOT: creating kafka producer")
 	p, err := producer.NewKafkaProducer(cfg, logger)
 	if err != nil {
-		logger.Fatal(err)
+		logger.Fatalf("BOOT: kafka producer error: %v", err)
 	}
+	logger.Println("BOOT: kafka producer created")
 
+	logger.Println("BOOT: creating batch worker pool")
 	wp := batch.NewWorkerPool(cfg, q, p, logger)
 
 	ctx, cancel := context.WithCancel(context.Background())
+
+	logger.Println("BOOT: starting batch workers")
 	go wp.Start(ctx)
 
+	logger.Println("BOOT: starting HTTP server")
 	srv := ingress.NewHTTPServer(cfg.HTTPAddr, q, logger)
-	go srv.Start()
+	go func() {
+		if err := srv.Start(); err != nil {
+			logger.Printf("HTTP: server exited: %v", err)
+		}
+	}()
+
+	logger.Printf("BOOT: injector ready, listening on %s\n", cfg.HTTPAddr)
+
+	// ---- SIGNAL HANDLING ----
 
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
-	<-sig
+
+	s := <-sig
+	logger.Printf("SHUTDOWN: received signal %v\n", s)
 
 	cancel()
+	logger.Println("SHUTDOWN: batch workers stopping")
 	wp.Stop()
 
 	ctxShutdown, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	logger.Println("SHUTDOWN: http server shutting down")
 	srv.Shutdown(ctxShutdown)
+
+	logger.Println("SHUTDOWN: kafka producer closing")
 	p.Close()
+
+	logger.Println("SHUTDOWN: complete")
 }
